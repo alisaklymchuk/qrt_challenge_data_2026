a45b5b58eb9b302ccf08a23ee592d9404b11007c
Danylo Dubas <dd1123@ic.ac.uk>
2026-09-24 16:14:14 +0100

    fixed model registry

--- src/models/model_utils.py
+++ src/models/model_utils.py
@@ -1,6 +1,6 @@
 from sklearn.metrics import make_scorer
 from . import MODEL_REGISTRY, add_model
-from . import ridge  # Import ridge model to register it
+from . import ridge, linear_mixed_model  # Import ridge model to register it
 
 def sign_acc(y_true, y_pred):
     return ((y_pred >= 0) == (y_true >= 0)).mean()
